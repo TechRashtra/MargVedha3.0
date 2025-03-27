b9a4bb8954188e14ce856f691337dfadee257b9d
Tanvi Mahale <tanvidmahale16@gmail.com>
2025-03-27 18:25:36 +0530

    Update TrafficMonitoringMap.js
    Signed-off-by: Tanvi Mahale <tanvidmahale16@gmail.com>

--- src/pages/TrafficMonitoringMap.js
+++ src/pages/TrafficMonitoringMap.js
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-// Custom Camera Icon
+
 const cameraIcon = new L.Icon({
   iconUrl: "https://cdn-icons-png.flaticon.com/512/565/565547.png", // Camera Icon URL
   iconSize: [32, 32], // Size of the icon
@@ -11,7 +11,7 @@ const cameraIcon = new L.Icon({
   popupAnchor: [0, -32] // Popup position
 });
 
-// Define Intersections
+
 const intersections = [
   { name: "Vidya Vikas Circle", lat: 20.01021, lon: 73.76414 },
   { name: "Spectrum", lat: 20.00724, lon: 73.77148 },
@@ -19,19 +19,19 @@ const intersections = [
   { name: "Theatre", lat: 20.00587, lon: 73.76331 }
 ];
 
-// Define Shortest Path (Connecting Intersections)
+
 const shortestPath = intersections.map(point => [point.lat, point.lon]);
 
 const TrafficMonitoringMap = () => {
   return (
     <MapContainer center={[20.00724, 73.77148]} zoom={15} style={{ height: "400px", width: "100%" }}>
-      {/* OpenStreetMap Tiles (Free) */}
+      
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      {/* Draw Shortest Path */}
+     
       <Polyline positions={shortestPath} color="blue" weight={5} />
 
-      {/* Add Camera Icons at Each Intersection */}
+     
       {intersections.map((point, index) => (
         <Marker key={index} position={[point.lat, point.lon]} icon={cameraIcon}>
           <Popup>
